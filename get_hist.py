31d02d73671f3f186a587938757a629d517758dc
lpryszcz <l.p.pryszcz@gmail.com>
2017-02-10 12:12:40 +0100

    get_hist.py remove_dbSNP.py

---
+++ get_hist.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# Return histogram of each type of editing
+
+# USAGE: ./get_hist.py [-s snps.vcf] file1.txt [... fileN.txt]
+
+import os, sys
+import matplotlib.pyplot as plt
+
+def load_snps(handle):
+  """Return dictionary of chromosomes and set of snps positions"""
+  snps = {}
+  for l in handle:
+    if l.startswith('#'):
+      continue  
+    chrom, pos = l.split('\t')[:5]
+    if chrom not in snps:
+      snps[chrom] = {}
+    snps[chrom].add(int(pos))
+  return snps
+  
+def txt2changes(handle, snps, template="%s>%s%s"):
+  """Return dictionary of snps and their occurencies"""
+  snp2freq = {template%(a, b, s): [] for a in bases for b in bases 
+                                     for s in strands if a!=b}
+  for l in handle:
+    if l.startswith('#'):
+      continue
+    try:
+      chrom, pos, strand, ref, alt, refcov, refQ, reffreq, altcov, altQ, altfreq = l[:-1].split('\t')
+      altfreq = float(altfreq)
+    except:
+      sys.stderr.write("[WARNING] Wrong line %s\n"%str(l[:-1].split('\t')))
+      break
+    # skip if known snp
+    if chrom in snps and int(pos) in snps[chrom]:
+      continue
+    snp2freq[template%(ref, alt, strand)].append(altfreq)
+  return snp2freq
+
+
+bases = "ACGT"
+strands = "+-"
+bins = 20
+base2rc= {"A": "T", "T": "A", "C": "G", "G": "C", ">": ">", "+": "-", "-": "+"}
+
+if sys.argv[1]=="-s ":
+  snps = load_snps(open(sys.argv[2]))
+  fnames = sys.argv[3:]
+else:
+  snps = {}
+  fnames = sys.argv[1:]
+  
+# process all files
+for i, fn in enumerate(fnames, 1):
+  fig = plt.figure(figsize=(15, 15)) # figsize=(24,16)) # figsize=(11.69,8.27)) #
+  print i, fn
+  outfn = "%s.hist.png"%fn
+  if os.path.isfile(outfn):
+    continue
+  # get snps  
+  snp2freq = txt2changes(open(fn), snps)
+  if not sum(len(x) for x in snp2freq.values()):
+    sys.stderr.write(" No editing found!\n")
+    continue
+  # plot hist
+  for ii, snp in enumerate(sorted(filter(lambda x: x[-1]=="+", snp2freq)), 1):
+    ax = fig.add_subplot(4, 3, ii)
+    snprc = "".join(base2rc[b] for b in snp)#; print snp, snprc
+    n, bins, patches = ax.hist([snp2freq[snp], snp2freq[snprc]], bins, normed=0, stacked=True, color=['blue', 'red',], label=["+", "-"])
+    ax.set_title(snp[:-1])
+    ax.grid(True)
+  plt.savefig(outfn, dpi=300, transparent=False) #orientation='landscape', 
+  
+  
---
+++ remove_dbSNP.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# Strip snps present in dbSNP
+# USAGE: ./remove_dbSNP.py 00-common_all.vcf.gz file1 [file2 ... fileN]
+
+import os, sys, gzip
+
+vcf = sys.argv[1]
+fnames = sys.argv[2:]
+
+def load_dbSNP(handle, add_chr=1):
+  """Return dictionary of SNP chromosomes and positions"""
+  snps = {}
+  j = k = 0
+  for i, l in enumerate(handle, 1):
+    #if i>1000: break
+    if l.startswith('#'):
+      continue
+    j += 1
+    chrom, pos, rs, ref, alt = l[:-1].split('\t')[:5]
+    if len(ref)!=len(alt):
+      continue
+    # 1 -> chr1
+    if add_chr and not chrom.startswith('chr'): 
+      chrom = "chr%s"%chrom
+    if chrom not in snps:
+      snps[chrom] = set()
+    snps[chrom].add(int(pos))
+    k += 1
+  print "Loaded %s out of %s SNPs"%(k, j)
+  return snps
+  
+def parse_editing(fn, snps, out):
+  """Ignore positions with known SNPs"""
+  j = k = 1
+  for l in open(fn):
+    if l.startswith("#"):
+      out.write(l)
+      continue
+    if len(l[:-1].split('\t'))<5:
+      continue
+    j += 1
+    chrom, pos, strand, ref, alt = l[:-1].split('\t')[:5]
+    if chrom in snps and int(pos) in snps[chrom]:
+      continue
+    out.write(l)
+    k += 1
+  return j, k
+  
+snps = load_dbSNP(gzip.open(vcf))
+
+for i, fn in enumerate(fnames, 1):
+  outfn = fn+".parsed.txt"
+  #if os.path.isfile(outfn):
+  #  continue
+  out = open(outfn, "w")
+  j, k = parse_editing(fn, snps, out)
+  print fn, k, j
+  
